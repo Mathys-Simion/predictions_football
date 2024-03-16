f4312ddb19fbfaa315c6a567d3db88c357e45a2c
foutejordan <2ftech20@gmail.com>
2024-03-16 20:46:30 +0100

    ✨ import data as a dataframe

--- src/import_data.py
+++ src/import_data.py
@@ -5,6 +5,7 @@ import requests
 
 # API settings
 BASE_URL = os.getenv('API_FOOTBALL_URL')
+
 HEADERS = {
 	"X-RapidAPI-Key": os.getenv('API_FOOTBALL_KEY'),
 	"X-RapidAPI-Host": os.getenv('API_FOOTBALL_HOST')
@@ -49,6 +50,7 @@ endpoints = {
 def fetch_data(endpoint):
     nb_requests = NB_MAX_DAILY_API_REQUESTS
     data_type = endpoint
+    print(data_type)
     filename = f'{DATA_FILENAME_PREFIX}{data_type["filename"]}{DATA_FILENAME_SUFFIX}'
     query_string = data_type['query_string']
     url = f'{BASE_URL}{data_type["url"]}'
@@ -62,9 +64,16 @@ def fetch_data(endpoint):
      with open(filename, 'w') as outfile:
         print(f'[INFO] -- {datetime.now()} -- saving data in {filename}')
         outfile.write(serialized_data)
-     with open(LOGS_NB_DAILY_REQUESTS_FILENAME, 'r') as logfile:
-         content = logfile.readline()
-         nb_requests = int(content)
+
+     nb_requests = 0
+     try:
+         with open(LOGS_NB_DAILY_REQUESTS_FILENAME, 'r') as logfile:
+             content = logfile.readline()
+             nb_requests = int(content)
+     except Exception as e:
+         with open(LOGS_NB_DAILY_REQUESTS_FILENAME, 'w+') as logfile:
+             logfile.write('0')
+
      with open(LOGS_NB_DAILY_REQUESTS_FILENAME, 'w+') as logfile:
          print(f'[INFO] -- {datetime.now()} -- {nb_requests + 1} requests made today')
          logfile.write(str(nb_requests + 1))
@@ -79,40 +88,32 @@ def fetch_data(endpoint):
 
 def fetch_fixtures(league_id, season):
     endpoint = {
-            'fixtures': {
             'filename': f'fixtures_for_league_{league_id}_and_season_{season}',
-            'query_string': { 'league': league_id, 'season': season },
+            'query_string': {'league': league_id, 'season': season},
             'url': 'fixtures'
-            }
             }
     return fetch_data(endpoint)
 
 def fetch_teams(team_id):
     endpoint = {
-      'teams': {
-          'filename': f'team_{team_id}',
-          'query_string': { 'id': team_id },
-          'url': 'teams'
-        },
+        'filename': f'team_{team_id}',
+        'query_string': {'id': team_id},
+        'url': 'teams'
     }
     return fetch_data(endpoint)
 
 def fetch_timezone():
     endpoint = {
-        'timezone': {
-            'filename': 'timezones',
-            'query_string': {},
-            'url': 'timezone'
-        }
+         'filename': 'timezones',
+        'query_string': {},
+        'url': 'timezone'
     }
     return fetch_data(endpoint)
 
 def fetch_statistics(team_id, league_id, season):
     endpoint = {
-        'statistics': {
         'filename': f'team_{team_id}_statistics_for_league_{league_id}_and_season_{season}',
         'query_string': { 'league': league_id, 'season': season, 'team': team_id },
         'url': 'teams/statistics'
-        }
     }
     return fetch_data(endpoint)
--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import os
 DATA_TYPE = os.getenv('DATA_TYPE', 'fixtures')
 LEAGUE_ID = os.getenv('LEAGUE_ID', '39')
 SEASON = os.getenv('SEASON', '2024')
+DATA_FOLDER = os.getenv('DATA_FOLDER')
 # TEAM_ID = os.getenv('TEAM_ID', '33')
 
 if __name__ == '__main__':
@@ -23,21 +24,43 @@ if __name__ == '__main__':
         'loses_team_1': [],
         'wins_team_2': [],
         'draws_team_2': [],
-        'loses_team_2': []
+        'loses_team_2': [],
+        'is_team1_winner': []
     }
-    df = pd.DataFrame()
-    i = 0
+
+    technical_id = 0
     for fixture_container in fixture_containers:
         # print('fixture', fixture)
-        dict_fixtures['technical_id'].append(i)
+        dict_fixtures['technical_id'].append(technical_id)
         dict_fixtures['id_fixture'].append(fixture_container['fixture']['id'])
         id_team_1 = fixture_container['teams']['home']['id']
         dict_fixtures['id_team_1'].append(id_team_1)
         id_team_2 = fixture_container['teams']['away']['id']
         dict_fixtures['id_team_2'].append(id_team_2)
-        dict_fixtures['wins_team_1'].append(fixture_container['teams']['home']['id'])
-        data = fetch_statistics(id_team_1, LEAGUE_ID, SEASON)
-        data_team_1 = fetch_statistics()
-        i += 1
-    print('nb fixtures', i)
+
+        data_team_1 = fetch_statistics(id_team_1, LEAGUE_ID, SEASON)
+        data_team_2 = fetch_statistics(id_team_2, LEAGUE_ID, SEASON)
+
+        dict_fixtures['wins_team_1'].append(data_team_1['response']['fixtures']['wins']['total'])
+        dict_fixtures['draws_team_1'].append(data_team_1['response']['fixtures']['draws']['total'])
+        dict_fixtures['loses_team_1'].append(data_team_1['response']['fixtures']['loses']['total'])
+
+        dict_fixtures['wins_team_2'].append(data_team_2['response']['fixtures']['wins']['total'])
+        dict_fixtures['draws_team_2'].append(data_team_2['response']['fixtures']['draws']['total'])
+        dict_fixtures['loses_team_2'].append(data_team_2['response']['fixtures']['loses']['total'])
+
+        winner = fixture_container['teams']['home']['winner']
+        dict_fixtures['is_team1_winner'].append("0" if winner is None else "1" if winner else "2")
+
+        technical_id += 1
+
+    df_fixtures = pd.DataFrame(dict_fixtures)
+    df_fixtures.to_csv(f'{DATA_FOLDER}/dataframe_fixtures_{LEAGUE_ID}_{SEASON}.csv', index=False)
+
+    print('nb fixtures', len(df_fixtures))
+    print(df_fixtures.head())
+    print(df_fixtures[df_fixtures['id_team_1'] == 44].value_counts())
+    print(df_fixtures[df_fixtures['id_team_2'] == 44].value_counts())
+    print(df_fixtures['is_team1_winner'].value_counts())
+
     print(f'[INFO] -- {datetime.now()} -- ending program')
